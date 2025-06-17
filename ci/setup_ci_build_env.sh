240b4285bd7ecc72a5eb9531218318f4732ee546
Radim Karniš <radim.karnis@espressif.com>
2025-06-17 14:05:13 +0200

    feat(toolchain): Update toolchain to esp-15.1.0_20250607

--- ci/setup_ci_build_env.sh
+++ ci/setup_ci_build_env.sh
@@ -3,10 +3,8 @@
 set -exuo pipefail
 
 ESP8266_TOOLCHAIN_URL="https://dl.espressif.com/dl/xtensa-lx106-elf-linux64-1.22.0-92-g8facf4c-5.2.0.tar.gz"
-ESP32_TOOLCHAIN_URL="https://github.com/espressif/crosstool-NG/releases/download/esp-2022r1/xtensa-esp32-elf-gcc11_2_0-esp-2022r1-linux-amd64.tar.xz"
-ESP32S2_TOOLCHAIN_URL="https://github.com/espressif/crosstool-NG/releases/download/esp-2022r1/xtensa-esp32s2-elf-gcc11_2_0-esp-2022r1-linux-amd64.tar.xz"
-ESP32S3_TOOLCHAIN_URL="https://github.com/espressif/crosstool-NG/releases/download/esp-2022r1/xtensa-esp32s3-elf-gcc11_2_0-esp-2022r1-linux-amd64.tar.xz"
-ESP32C3_TOOLCHAIN_URL="https://github.com/espressif/crosstool-NG/releases/download/esp-2022r1/riscv32-esp-elf-gcc11_2_0-esp-2022r1-linux-amd64.tar.xz"
+XTENSA_TOOLCHAIN_URL="https://github.com/espressif/crosstool-NG/releases/download/esp-15.1.0_20250607/xtensa-esp-elf-15.1.0_20250607-x86_64-linux-gnu.tar.xz"
+RISCV_TOOLCHAIN_URL="https://github.com/espressif/crosstool-NG/releases/download/esp-15.1.0_20250607/riscv32-esp-elf-15.1.0_20250607-x86_64-linux-gnu.tar.xz"
 
 # Setup shell script to download & configure ESP8266 & ESP32 toolchains
 # for building the flasher stub program
@@ -19,26 +17,14 @@ if ! [ -d ${ESP8266_BINDIR} ]; then
     tar zxf $(basename ${ESP8266_TOOLCHAIN_URL})
 fi
 
-if ! [ -d ${ESP32_BINDIR} ]; then
+if ! [ -d ${XTENSA_BINDIR} ]; then
     # gitlab CI image may already have this file
-    wget --continue --no-verbose "${ESP32_TOOLCHAIN_URL}"
-    tar Jxf $(basename ${ESP32_TOOLCHAIN_URL})
+    wget --continue --no-verbose "${XTENSA_TOOLCHAIN_URL}"
+    tar Jxf $(basename ${XTENSA_TOOLCHAIN_URL})
 fi
 
-if ! [ -d ${ESP32S2_BINDIR} ]; then
+if ! [ -d ${RISCV_BINDIR} ]; then
     # gitlab CI image may already have this file
-    wget --continue --no-verbose "${ESP32S2_TOOLCHAIN_URL}"
-    tar Jxf $(basename ${ESP32S2_TOOLCHAIN_URL})
-fi
-
-if ! [ -d ${ESP32S3_BINDIR} ]; then
-    # gitlab CI image may already have this file
-    wget --continue --no-verbose "${ESP32S3_TOOLCHAIN_URL}"
-    tar Jxf $(basename ${ESP32S3_TOOLCHAIN_URL})
-fi
-
-if ! [ -d ${ESP32C3_BINDIR} ]; then
-    # gitlab CI image may already have this file
-    wget --continue --no-verbose "${ESP32C3_TOOLCHAIN_URL}"
-    tar Jxf $(basename ${ESP32C3_TOOLCHAIN_URL})
+    wget --continue --no-verbose "${RISCV_TOOLCHAIN_URL}"
+    tar Jxf $(basename ${RISCV_TOOLCHAIN_URL})
 fi
